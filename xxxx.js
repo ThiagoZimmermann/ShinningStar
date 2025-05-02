cd55c89b6abd0a2349fd51d80962b07bf827208a
ThiagoZimmermann <czthiago47@outlook.com>
2025-05-02 02:44:29 -0300

    Remove alert messages from Menu function for cleaner navigation

--- xxxx.js
+++ xxxx.js
@@ -1,27 +1,21 @@
 function Menu(id) {
     switch (id) {
         case "logo":
-            alert("Clicou no logo");
             window.location.href = "index.html";
             break;
         case "prod":
-            alert("Clicou em Produtos");
             window.location.href = "Produtos/prod.html";
             break;
         case "seg":
-            alert("Clicou em Segurança");
             window.location.href = "Seguranca/seg.html";
             break;
         case "about":
-            alert("Clicou em Sobre nós");
             window.location.href = "Sobre nos/sobre.html";
             break;
         case "en":
-            alert("Clicou em English");
             window.location.href = "English/english.html";
             break;
         case "login_bttn":
-            alert("Clicou em Entrar");
             window.location.href = "Entrar/entrar.html";
             break;
         default:
