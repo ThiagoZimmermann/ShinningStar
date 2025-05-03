b845e39226c7389968426ce0a51d6ed6f8b35701
ThiagoZimmermann <czthiago47@outlook.com>
2025-05-03 01:32:25 -0300

    Fix file paths for images and links in HTML and JavaScript files for consistent navigation

--- English/english.js
+++ English/english.js
@@ -1,22 +1,22 @@
 function Menu(id) {
     switch (id) {
         case "logo":
-            window.location.href = "/index.html";
+            window.location.href = "../index.html";
             break;
         case "prod":
-            window.location.href = "/Produtos/prod.html";
+            window.location.href = "../Produtos/prod.html";
             break;
         case "seg":
-            window.location.href = "/Seguranca/seg.html";
+            window.location.href = "../Seguranca/seg.html";
             break;
         case "about":
-            window.location.href = "/Sobre nos/sobre.html";
+            window.location.href = "../Sobre nos/sobre.html";
             break;
         case "en":
             window.location.href = "../English/english.html";
             break;
         case "login_bttn":
-            window.location.href = "/Entrar/entrar.html";
+            window.location.href = "../Entrar/entrar.html";
             break;
         default:
             alert("Opção desconhecida");
